699438598983244fbf17caa328ad3bf69d1000b6
Thiago Rodrigues <thiagorodriguessa7@gmail.com>
2025-07-24 13:01:07 -0300

    Adição do for com base na contagem, parâmetros recebendo o numero do terminal, e lançando a exceção

--- src/Contador.java
+++ src/Contador.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro:");
+        System.out.println("Digite o primeiro parâmetro");
         int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro:");
+        System.out.println("Digite o segundo parâmetro");
         int parametroDois = terminal.nextInt();
 
 
